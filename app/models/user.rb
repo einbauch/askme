21ff1a7d462cacfa02fe52e7ba9de394f6af58dd
Alexander Kuzmin <alex.kuzminx@gmail.com>
2016-06-01 01:27:46 -0700

    lesson finished state

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,46 @@
+require 'openssl'
+
 class User < ActiveRecord::Base
+
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+
+
+  has_many :questions
+  validates :email, :username, presence: true
+  validates :email, :username, uniqueness: true
+
+  attr_accessor :password
+
+  validates :password, presence: true, on: :create
+  validates_confirmation_of :password
+
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      # Creating salt
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      # Creating password hash
+      self.password_hash = User.hash_to_string(
+          OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  # converting a binary string into HEX
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
+
 end
