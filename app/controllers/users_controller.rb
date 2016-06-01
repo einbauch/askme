7550689a9fff6eaeffcdbf33935a08efb15d3b51
Alexander Kuzmin <alex.kuzminx@gmail.com>
2016-06-01 14:23:01 -0700

    add question count for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,5 +34,29 @@ class UsersController < ApplicationController
     ]
 
     @new_question = Question.new
+
+    @question_count = sklonyator(@questions.length, 'вопрос', 'вопроса', 'вопросов', true)
+  end
+
+  def sklonyator (number, krokodil, krokodila, krokodilov, with_number = false)
+    if number == nil || !number.is_a?(Numeric)
+      number = 0
+    end
+
+    prefix = ''
+    prefix = "#{number} " if with_number
+
+    ostatok = number % 10
+    ostatok100 = number % 100
+
+    if ostatok100 >= 11 && ostatok100 <=14
+      return "#{prefix}#{krokodilov}"
+    elsif ostatok == 1
+      return "#{prefix}#{krokodil}"
+    elsif ostatok >= 2 && ostatok <= 4
+      return "#{prefix}#{krokodila}"
+    elsif ostatok > 4 || ostatok == 0
+      return "#{prefix}#{krokodilov}"
+    end
   end
 end
