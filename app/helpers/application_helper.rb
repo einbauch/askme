a2f6efc10238deea8d6c5afa7dfac71756d8c947
Alexander Kuzmin <alex.kuzminx@gmail.com>
2016-06-03 13:23:56 -0700

    move sklonyator to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,4 +12,26 @@ module ApplicationHelper
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
 
+  def sklonyator (number, krokodil, krokodila, krokodilov, with_number = false)
+    if number == nil || !number.is_a?(Numeric)
+      number = 0
+    end
+
+    prefix = ''
+    prefix = "#{number} " if with_number
+
+    ostatok = number % 10
+    ostatok100 = number % 100
+
+    if ostatok100 >= 11 && ostatok100 <=14
+      return "#{prefix}#{krokodilov}"
+    elsif ostatok == 1
+      return "#{prefix}#{krokodil}"
+    elsif ostatok >= 2 && ostatok <= 4
+      return "#{prefix}#{krokodila}"
+    elsif ostatok > 4 || ostatok == 0
+      return "#{prefix}#{krokodilov}"
+    end
+  end
+
 end
