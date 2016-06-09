fadc9a3dce072fdfe965c8975f0a93bfeb9aab86
Alex Kuzmin <alex.kuzmin@bk.ru>
2016-06-10 01:18:54 +0300

    add text color change in user page depending on background color (apart from link)

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,2 +1,15 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+    function isDark( color ) {
+        var match = /rgb\((\d+).*?(\d+).*?(\d+)\)/.exec(color);
+        return ( match[1] & 255 )
+            + ( match[2] & 255 )
+            + ( match[3] & 255 )
+            < 3 * 256 / 2;
+    }
+
+    $('.user-header').each(function() {console.log($(this).css("background-color"));
+        $(this).css("color", isDark($(this).css("background-color")) ? 'white' : 'black');
+    });
+});
